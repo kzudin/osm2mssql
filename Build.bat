332f4e55e3db0e3f3c12b034f921bebc8ce3fb7a
Konstantin.Zudin <Konstantin.Zudin@local.local>
2019-12-04 09:54:46 +0300

    Изменение файла сборки;

--- Build.bat
+++ Build.bat
@@ -5,8 +5,8 @@ REM Устанавливаем пути к программам
 SET MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
 SET VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 IF EXIST %VSWHERE% (
-	FOR /f "usebackq tokens=*" %%i in (`%VSWHERE% -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) DO (
-		SET MSBUILD="%%i\MSBuild\15.0\Bin\MSBuild.exe"
+	FOR /f "usebackq tokens=*" %%i in (`%VSWHERE% -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) DO (
+		SET MSBUILD="%%i"
 	)
 )
 SET PATHCMD=%~dp0
